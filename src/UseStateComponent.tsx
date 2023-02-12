fd42c0102465ebf538c3339d6ff9a7dd4522bc56
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-11 17:25:08 -0700

    Created another useState example

--- src/UseStateComponent.tsx
+++ src/UseStateComponent.tsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 const UseStateComponent = () => {
   const [arr, arrSet] = useState<number[]>([])
+  const [name, nameSet] = useState<string | null>(null)
 
   return (
     <div>
-    <div>
-      <button onClick={() => arrSet([...arr, arr.length + 1])}> Add to Array</button>
-      {JSON.stringify(arr)}
+      <div>
+        <button onClick={() => nameSet("Jack")}> Set Name </button>
+        {name}
+      </div>
+      <div>
+        <button onClick={() => arrSet([...arr, arr.length + 1])}> Add to Array</button>
+        {JSON.stringify(arr)}
+      </div>
     </div>
-  </div>
   )
 }
 
