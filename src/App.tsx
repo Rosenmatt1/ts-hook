893d19aa3660c4bb5ec92c0af76279b964cd8063
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-11 19:18:11 -0700

    Created useEffectComponent with timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import UseStateComponent from "./UseStateComponent";
+import UseEffectComponent from "./UseEffectComponent";
 
 function App() {
   return (
     <div>
       <h1>useState</h1>
+      <UseEffectComponent />
       <UseStateComponent />
     </div>
   );
