6f41427c5f657d0f6163cc5207de2de6b4325289
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-12 22:20:58 -0700

    Created a componenrt with two functions to show useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
 import UseStateComponent from "./UseStateComponent";
 import UseEffectComponent from "./UseEffectComponent";
+import UseContextComponent from "./UseContextComponent";
+import UseReducerComponent from "./UseReducerComponent"
 
 function App() {
   return (
     <div>
-      <h1>useState</h1>
+
+      <h1>useReducer</h1>
+      <UseReducerComponent />
+
+      <h1>useContext</h1>
+      <UseContextComponent />
+
+      <h1>useEffect</h1>
       <UseEffectComponent />
+
+      <h1>useState</h1>
       <UseStateComponent />
     </div>
   );
